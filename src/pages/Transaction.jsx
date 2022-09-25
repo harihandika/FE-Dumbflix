c8ec7c2b3bc2e5751dce75218b88ca577910cbd8
Hari Handika Setiawan <harihandika456@gmail.com>
2022-09-26 00:22:39 +0700

    FE-Dumbflix

--- src/pages/Transaction.jsx
+++ src/pages/Transaction.jsx
@@ -74,7 +74,7 @@ function Transaction() {
                           <td>{item.user.email}</td>
                           {item.status === "success"?
                           <td className='text-success'>{Duration(item.duedate, item.startdate)}</td>:<td className='text-success'>30 day</td>}
-                          <td className={item.status === "success" ? "text-success" : "text-danger"}>{item.status === "success" ? "Active" : "Not Active"}</td>
+                          <td className={item.status === "success" ? "text-success" : "text-success"}>{item.status === "success" ? "Active" : "Active"}</td>
                           <td className={item.status === "success" ? "text-success" : item.status === "pending" ? "text-warning" : "text-success"}>{item.status === "success" ? "Success" : item.status === "pending" ? "Pending" : "Success"}</td>
                         </tr>
                       </>
